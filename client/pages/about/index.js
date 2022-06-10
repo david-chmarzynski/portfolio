d864f700457f3a96eeb15a3dc3b872c1820d7d16
David Chmarzynski <david.chmarzynski@epitech.eu>
2022-06-10 16:50:05 +0200

    update 10/06/22

--- client/pages/about/index.js
+++ client/pages/about/index.js
@@ -1,61 +1,59 @@
 // IMPORT NEXT.JS & REACT COMPONENTS
-import { useEffect } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 // IMPORT GSAP & ANIMATIONS
-import gsap from 'gsap';
+import gsap from "gsap";
 
 // IMPORT ASSETS
 import { ArrowRightOutlined } from "@ant-design/icons";
 
-
 export default function About() {
-
   useEffect(() => {
-    gsap.from('.about-title', {
+    gsap.from(".about-title", {
       opacity: 0,
       x: -200,
-      ease: 'power4.out',
+      ease: "power4.out",
       duration: 1,
       delay: 1,
       stagger: {
-        amount: 0.3
-      }
+        amount: 0.3,
+      },
     });
 
-    gsap.from('.about-subtitle', {
+    gsap.from(".about-subtitle", {
       opacity: 0,
       y: 100,
       skewY: 7,
-      ease: 'power4.out',
+      ease: "power4.out",
       duration: 1,
       delay: 1.3,
       stagger: {
-        amount: 0.3
-      }
+        amount: 0.3,
+      },
     });
 
-    gsap.from('p', {
+    gsap.from("p", {
       opacity: 0,
       x: -100,
-      ease: 'power4.out',
+      ease: "power4.out",
       duration: 1,
       delay: 2,
       stagger: {
-        amount: 0.3
-      }
+        amount: 0.3,
+      },
     });
 
-    gsap.from('a', {
+    gsap.from("a", {
       opacity: 0,
       x: -100,
-      ease: 'power4.out',
+      ease: "power4.out",
       duration: 1,
       delay: 2,
       stagger: {
-        amount: 0.3
-      }
+        amount: 0.3,
+      },
     });
   }, []);
 
@@ -68,23 +66,24 @@ export default function About() {
       <div className="about-container">
         <div className="about-left-layer">
           <h1 className="about-title">
-            DAVID<br/>CHMARZYNSKI
+            DAVID
+            <br />
+            CHMARZYNSKI
           </h1>
         </div>
         <div className="about-right-layer">
-          <h2 className="about-subtitle">
-            BIO
-          </h2>
+          <h2 className="about-subtitle">BIO</h2>
           <div className="about-description">
             <p>
               Basé à Marseille, je suis un développeur web fullstack Javascript
-              créatif passionné par le design et les animations.<br/>
-              Futur étudiant Epitech, je suis à la recherche d'une entreprise 
-              accueillant un alternant.</p>
+              créatif passionné par le design et les animations.
+              <br />
+              Étudiant Epitech en première année de Master dès le 12 Septembre
+              2022, je suis à la recherche d'une entreprise accueillant un
+              alternant.
+            </p>
           </div>
-          <h2 className="about-subtitle">
-            TECHNOS
-          </h2>
+          <h2 className="about-subtitle">TECHNOS</h2>
           <div className="about-technos">
             <div className="about-technos-part-1">
               <p>HTML</p>
@@ -105,45 +104,43 @@ export default function About() {
               <p>SQL/NoSQL</p>
               <p>Adobe XD</p>
               <p>Production</p>
-              <p>PM2</p> 
+              <p>PM2</p>
             </div>
           </div>
-          <h2 className="about-subtitle">
-            SOCIAL
-          </h2>
+          <h2 className="about-subtitle">SOCIAL</h2>
           <div className="about-social">
             <div className="about-link">
               <Link href="mailto: david.chmarzynski@gmail.com">
-                <a className="about-email" target="_blank">EMAIL</a>
+                <a className="about-email" target="_blank">
+                  EMAIL
+                </a>
               </Link>
               <ArrowRightOutlined />
             </div>
             <div className="about-link">
               <Link href="https://github.com/david-chmarzynski">
-                <a className="about-github" target="_blank">GITHUB</a>
+                <a className="about-github" target="_blank">
+                  GITHUB
+                </a>
               </Link>
               <ArrowRightOutlined />
             </div>
             <div className="about-link">
               <Link href="https://www.linkedin.com/in/david-chmarzynski-3bab78190/">
-                <a className="about-linkedin" target="_blank">LINKEDIN</a>
+                <a className="about-linkedin" target="_blank">
+                  LINKEDIN
+                </a>
               </Link>
               <ArrowRightOutlined />
             </div>
           </div>
-          <h2 className="about-subtitle">
-            RECRUTEZ-MOI
-          </h2>
+          <h2 className="about-subtitle">RECRUTEZ-MOI</h2>
           <div className="about-hire">
-            <div className="about-epitech">
-              E
-            </div>
-            <div className="about-curriculum">
-              CV
-            </div>
+            <div className="about-epitech">E</div>
+            <div className="about-curriculum">CV</div>
           </div>
         </div>
       </div>
     </div>
-  )
-};
+  );
+}
